b34e147e2567c80f88b88df853602195b51fd44f
mhamzaalmas <mhamzaalmas101@gmail.com>
2025-07-28 16:28:03 +0500

    javascript file added

---
+++ srcipt.js
@@ -0,0 +1,27 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  let top = window.scrollY;
+
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+
+      let activeLink = document.querySelector(`header nav a[href="#${id}"]`);
+      if (activeLink) {
+        activeLink.classList.add('active');
+      }
+    }
+  });
+};
+
+
+// sticky header
